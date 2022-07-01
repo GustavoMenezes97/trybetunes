10291243f6de8a56cba0099d86dd8a6c472a4215
GustavoMenezes97 <gustavo.a.r.menezes@gmail.com>
2022-07-01 15:20:49 -0300

    Etapa 1 concluida

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import NotFound from './pages/NotFound';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
+import Profile from './pages/Profile';
+import Login from './pages/Login';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <p>TrybeTunes</p>
+        <div>
+          <Search />
+          <Album />
+          <Favorites />
+          <ProfileEdit />
+          <Profile />
+          <NotFound />
+          <Login />
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
