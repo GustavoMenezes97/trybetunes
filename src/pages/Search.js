869daeabdaa8226d68948c7882a1b22a1bb2726d
GustavoMenezes97 <gustavo.a.r.menezes@gmail.com>
2022-07-04 18:35:15 -0300

    Etapa 5 concluida

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,55 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artistName: '',
+      buttonSearch: true,
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    }, () => this.buttonValidations());
+  }
+
+  buttonValidations = () => {
+    const { artistName } = this.state;
+    const minValue = 2;
+    return artistName.length >= minValue
+      ? this.setState({ buttonSearch: false })
+      : this.setState({ buttonSearch: true });
+  }
+
   render() {
+    const { artistName, buttonSearch } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-search" />
+        <div data-testid="page-search">
+          <label htmlFor="artistName">
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              name="artistName"
+              id="artistName"
+              value={ artistName }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            name="buttonSearch"
+            id="buttonSearch"
+            disabled={ buttonSearch }
+          >
+            Search
+          </button>
+        </div>
       </>
     );
   }
