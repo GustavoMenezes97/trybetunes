ff9ef3883118112f2f1d2cea7e69128997c4d26b
GustavoMenezes97 <gustavo.a.r.menezes@gmail.com>
2022-07-04 16:54:31 -0300

    Etapa 2 concluida

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import Album from './pages/Album';
-import Favorites from './pages/Favorites';
-import NotFound from './pages/NotFound';
-import ProfileEdit from './pages/ProfileEdit';
-import Search from './pages/Search';
-import Profile from './pages/Profile';
-import Login from './pages/Login';
+import Content from './components/Content';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <p>TrybeTunes</p>
-        <div>
-          <Search />
-          <Album />
-          <Favorites />
-          <ProfileEdit />
-          <Profile />
-          <NotFound />
-          <Login />
-        </div>
+        <Content />
       </BrowserRouter>
     );
   }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,73 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userName: '',
+      buttonLogin: true,
+      loading: false,
+      loaded: false,
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    }, () => this.buttonValidation());
+  }
+
+  buttonValidation = () => {
+    const { userName } = this.state;
+    const minValue = 3;
+    return userName.length >= minValue
+      ? this.setState({ buttonLogin: false })
+      : this.setState({ buttonLogin: true });
+  }
+
+  buttonFunction = async (event) => {
+    event.preventDefault();
+    const { userName } = this.state;
+    this.setState({ loading: true });
+    await createUser({ name: userName });
+    this.setState({ loading: false, loaded: true });
+  }
+
   render() {
+    const { userName, buttonLogin, loading, loaded } = this.state;
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        {loaded && <Redirect to="/search" />}
+        {loading ? <Loading /> : (
+          <form>
+            <label htmlFor="userName">
+              User name:
+              <input
+                data-testid="login-name-input"
+                type="text"
+                name="userName"
+                id="userName"
+                placeholder="first and last name"
+                value={ userName }
+                onChange={ this.handleChange }
+              />
+            </label>
+            <button
+              data-testid="login-submit-button"
+              type="submit"
+              name="buttonLogin"
+              disabled={ buttonLogin }
+              onClick={ this.buttonFunction }
+            >
+              Entrar
+            </button>
+          </form>
+        )}
+      </div>
     );
   }
 }
